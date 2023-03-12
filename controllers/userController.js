0279ac4ae30462cfe62c102735c61bb276604a14
Aayushi <aayushiiiii09@gmail.com>
2023-03-12 12:16:01 +0530

    register updated & login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,102 +1,76 @@
 import bigPromise from '../middlewares/bigPromise.js'
 import User from '../models/User.js'
+import { mailHelper } from '../utils/mailHelper.js'
+import { cookieToken } from '../utils/cookieToken.js'
+import crypto from 'crypto'
 
-export const createUser = bigPromise(async (req, res, next) => {
+export const register = bigPromise(async (req, res, next) => {
   try {
-    const { username, email, password } = req.body
-    if (!username || !email || !password) {
+    const { username, email, password, role } = req.body
+    if (!username || !password || !email || !role) {
       return res
         .status(400)
-        .json({ success: false, message: 'Enter all the fields' })
+        .json({ success: false, message: 'all fields are required!' })
     }
-    //check if the user exists
-    const existingUser = await User.findOne({ email: email })
+    const existingUser = await User.findById({ email: email })
       .lean()
-      .catch((error) => {
-        console.log(`error is ${error}`)
+      .catch((ExistingUsererror) => {
+        console.log(
+          `Error while checking for existing user:: ${ExistingUsererror}`
+        )
       })
     if (existingUser) {
       return res
         .status(500)
-        .json({ success: true, message: 'User already Exits' })
+        .json({ success: false, message: 'User already exits!' })
     }
-    //create user
     const user = await User.create({ ...req.body })
     return res
       .status(200)
-      .json({ success: true, message: 'User created successfully', data: user })
+      .json({ succes: true, mesaage: 'User created successfully!', data: user })
   } catch (error) {
-    console.log(`Error while creating user: ${error}`)
+    console.log(`Error while registering/Creating a user:: ${error}`)
   }
 })
 
-export const deleteUser = bigPromise(async (req, res, next) => {
+export const login = bigPromise(async (req, res, next) => {
   try {
-    const id = req.params.id
-    const existingUser = await User.findByIdAndDelete({ _id: id })
-    if (!existingUser) {
+    const { username, password } = req.body
+    if (!username || !password) {
       return res
         .status(400)
-        .json({ success: false, message: 'no such user exists!' })
+        .json({ success: false, message: 'all fields are required!' })
     }
-    return res
-      .status(200)
-      .json({ success: true, message: 'user deleted successfully' })
-  } catch (error) {
-    console.log(`Error while deleting the user:: ${error}`)
-  }
-})
-
-export const updateUser = bigPromise(async (req, res, next) => {
-  try {
-    const Id = req.params.id
-    const existingUser = await User.findById({ _id: Id })
+    const existingUser = await User.findById({ username: username })
       .lean()
       .catch((error) => {
-        console.log(`error existing user:: ${error}`)
+        console.log(`Error while checking for existing user:: ${error}`)
       })
     if (!existingUser) {
-      return res
-        .status(400)
-        .json({ success: false, message: 'no such user exists!' })
-    }
-    const newData = {
-      username: req.body.username,
-      email: req.body.email,
-      role: req.body.role,
-    }
-    const updatedValue = await User.findByIdAndUpdate(req.params.id, newData, {
-      new: true,
-      runValidators: true,
-      useFindAndModify: false,
-    }).catch((error) => {
-      console.log(`error while updating :: ${error}`)
-      return null
-    })
-    console.log(updatedValue)
-    if (updatedValue === null) {
-      return res
-        .status(500)
-        .json({ success: false, message: 'Internal Server error' })
-    }
-
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: 'Updated successfully',
-        data: existingUser,
+      return res.status(500).json({
+        success: false,
+        message: 'No such User exists, Register first!',
       })
-      .send(updatedValue)
-  } catch (err) {
-    console.log(`error while updating a user:: ${err}`)
-  }
-})
+    }
+    const isPasswordCorrect = await existingUser.isValidatedPassword(
+      password,
+      existingUser.password
+    )
 
-export const retriveUser = bigPromise(async (req, res, next) => {
-  try {
+    if (!isPasswordCorrect) {
+      return res.status(401).json({
+        success: 'false',
+        message: 'Incorrect Password',
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'User logged in successfully!',
+      existingUser,
+    })
   } catch (error) {
-    console.log(`error while retriving the users:: ${error}`)
+    console.log(`error while login-ing:: ${error}`)
   }
 })
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,15 +2,13 @@ import express from 'express'
 const router = express.Router()
 
 // import controllers
-import {
-  createUser,
-  deleteUser,
-  updateUser,
-} from '../controllers/userController.js'
+import { login, register } from '../controllers/userController.js'
 
-router.route('/createUser').post(createUser)
-router.route('/deleteUser/:id').delete(deleteUser)
+router.route('/register').post(register)
+router.route('/login').post(login)
+// router.route('/createUser').post(createUser)
+// router.route('/deleteUser/:id').delete(deleteUser)
 // router.route('/retriveUser').get(retriveUser)
-router.route('/updateUser/:id').put(updateUser)
+// router.route('/updateUser/:id').put(updateUser)
 
 export default router
